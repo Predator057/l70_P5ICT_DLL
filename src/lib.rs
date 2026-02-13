0f20e29bba4cc92f90d3946b281491f5b171d384
Ivan <kozlov01ivan@gmail.com>
2026-02-13 16:48:07 +0300

    разбираем пакеты

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
-use std::time::Duration;
+use std::io::Read;
+use std::{fs, time::Duration};
 use std::thread::sleep;
 use windows::{Win32::{Foundation::*, System::SystemServices::*}, core::{s}};
 use serialport::{self, DataBits, Parity, StopBits};
@@ -36,54 +37,81 @@ extern "system" fn DllMain(dll_module: HINSTANCE, call_reason: u32, _: *mut ()) 
 #[unsafe(no_mangle)]
 #[allow(non_snake_case, unused_variables)]
 extern "C" fn InitAcceptor() -> i16{
-    let port_nm = String::from("COM6");
+    let mut port_nm = String::new();
+    match fs::File::open("acceptor"){
+        Ok(mut file)=>{
+            let port = file.read_to_string(&mut port_nm);
+        }
+        Err(e) => {
+            return -222
+        }
+    }
 
     let port_result = serialport::new(port_nm, 9600)
-        .timeout(Duration::from_millis(60))
+        .timeout(Duration::from_millis(20))
         .data_bits(DataBits::Eight)
         .parity(Parity::None)
         .stop_bits(StopBits::One)
         .open().map_err(|e|{
+            println!("error {}", e);
             return -2;
         });
     
     match port_result {
         Ok(mut port) => {
-            let buf: [u8; 1] = [RESET];
-            let _ = port.write(&buf);
-
-            loop{
-                let mut buffer = vec![0; 128];
+            let _ = port.write(&[RESET]);
+            for _ in 0..100 {
+                let mut buffer = [0u8; 4];
                 match port.read(buffer.as_mut_slice()){
-                    Ok(_)=>{
-                        for i in 0..128{
-                            if buffer[i] == 0x80 || buffer[i] == 0x8F{
-                                let buf: [u8; 1] = [ACCEPT];
-                                let _ = port.write(&buf);
-                                let buf: [u8; 1] = [DISABLE];
-                                let _ = port.write(&buf);
-                                return 0;
+                    Ok(n)=>{
+                        println!("buffer {:?}", buffer);
+                        for i in 0..buffer.len() {
+                            if buffer[i] == 0x80{
+                                let _ = port.write(&[ACCEPT]);
+                                println!("принято");
                             }
+                            if buffer[i] == 0x8F {
+                                let _ = port.write(&[ACCEPT]);
+                                println!("принято");
+                                sleep(Duration::from_millis(3000));
+                                let _ = port.write(&[DISABLE]);
+                                println!("отключение");
+                                println!("успешно");
+                                return 0;
+                            } 
                         }
+                        // sleep(Duration::from_millis(3000));
+                        // let _ = port.write(&[DISABLE]);
+                        // println!("отключение");
+                        // println!("успешно");
                     }
                     Err(_) =>{}
                 }
-                sleep(Duration::from_millis(100));
             }
+            return -3;
         }
         Err(e)=>{
-            return e
+            return -1;
         }
     }
+
 }
 
 #[unsafe(no_mangle)]
 #[allow(non_snake_case, unused_variables)]
 extern "C" fn StartAcceptor() -> i16{
-    let port_nm = String::from("COM6");
+    let mut port_nm = String::new();
+    match fs::File::open("acceptor"){
+        Ok(mut file)=>{
+            let port = file.read_to_string(&mut port_nm);
+        }
+        Err(e) => {
+            return -222
+        }
+    }
 
     let port_result = serialport::new(port_nm, 9600)
-        .timeout(Duration::from_millis(60))
+        .timeout(Duration::from_millis(20))
         .data_bits(serialport::DataBits::Eight)
         .parity(Parity::None)
         .stop_bits(serialport::StopBits::One)
@@ -93,60 +121,57 @@ extern "C" fn StartAcceptor() -> i16{
 
     match port_result {
         Ok(mut port) =>{
-            
-            let buf: [u8; 1] = [ENABLE];
-            let _ = port.write(&buf);
+            let mut rx_buffer: Vec<u8> = Vec::new();
+            let _ = port.write(&[ENABLE]);
             loop{
                 //01 - 50  руб
                 //02 - 100 руб
                 //03 - 500 руб
                 //04 - 1000 руб
-                let mut buffer = vec![0; 128];
-                match port.read(buffer.as_mut_slice()){
-                    Ok(_)=>{
-                        for i in 0..128{
-                            if buffer[i] == 0x80 || buffer[i] == 0x8F{
-                                let buf: [u8; 1] = [0x02];
-                                let _ = port.write(&buf);
-                            }
-                            match buffer[i]{
-                                0x41 => {
-                                    let buf: [u8; 1] = [0x02];
-                                    let _ = port.write(&buf);
-                                    return 50;
-                                }
-                                0x42 => {
-                                    let buf: [u8; 1] = [0x02];
-                                    let _ = port.write(&buf);
-                                    return 100;
-                                }
-                                0x43 => {
-                                    let buf: [u8; 1] = [0x02];
-                                    let _ = port.write(&buf);
-                                    return 500;
-                                }
-                                0x44 => {
-                                    let buf: [u8; 1] = [0x02];
-                                    let _ = port.write(&buf);
-                                    return 1000;
-                                }
-                                0x45 => {
-                                    let buf: [u8; 1] = [0x02];
-                                    let _ = port.write(&buf);
-                                    return 5000;
-                                }
-                                0x81 => {
-                                    let buf: [u8; 1] = [0x02];
-                                    let _ = port.write(&buf);
-                                }
-                                _ => {
-                                    return -1;
+                let mut temp = [0u8; 2];
+                match port.read(temp.as_mut_slice()){
+                    Ok(n)=>{
+                        rx_buffer.extend_from_slice(&temp[..n]);
+                        println!("буфер купюры {:?}", rx_buffer);
+                        if rx_buffer.len() < 2{
+                            sleep(Duration::from_millis(100));
+                            continue;
+                        }
+                        for i in 0..temp.len(){
+                            if rx_buffer[0] == 0x81{
+                                match rx_buffer[1] {
+                                    0x41 => {
+                                        let _ = port.write(&[0x02]);
+                                        return 50;
+                                    }
+                                    0x42 => {
+                                        let _ = port.write(&[0x02]);
+                                        return 100;
+                                    }
+                                    0x43 => {
+                                        let _ = port.write(&[0x02]);
+                                        return 500;
+                                    }
+                                    0x44 => {
+                                        let _ = port.write(&[0x0F]);
+                                        continue;
+                                    }
+                                    0x45 => {
+                                        let _ = port.write(&[0x0F]);
+                                        continue;
+                                    }
+                                    _ => {
+                                        let _ = port.write(&[0x0F]);
+                                        continue;
+                                    }
                                 }
                             }
                         }
+                        rx_buffer.clear();
                     }
-                    Err(_) =>{}
+                    Err(_) =>{rx_buffer.clear()}
                 }
+                sleep(Duration::from_millis(100));
             }
         }
         Err(e) => {
@@ -159,7 +184,15 @@ extern "C" fn StartAcceptor() -> i16{
 #[unsafe(no_mangle)]
 #[allow(non_snake_case, unused_variables)]
 extern "C" fn DisableAcceptor() -> i16{
-    let port_nm = String::from("COM6");
+    let mut port_nm = String::new();
+    match fs::File::open("acceptor"){
+        Ok(mut file)=>{
+            let port = file.read_to_string(&mut port_nm);
+        }
+        Err(e) => {
+            return -222
+        }
+    }
 
     let port_result = serialport::new(port_nm, 9600)
         .timeout(Duration::from_millis(60))
